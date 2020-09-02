002529e6cb9bad96da1e991efa72080ad8a1bd15
aktaktaw <aktaktaw@gmail.com>
2020-09-02 12:56:00 +0800

    complete project vue calculator

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calculator/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculator from '@/components/Calculator'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Calculator
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  font-size: 3em;
+}
+body{
+  width: 100vw;
+  height: 100vh;
+  font-size: 2.5vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
